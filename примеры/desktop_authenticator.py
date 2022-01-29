de62687fe2e23bf1351b74878770e414ed6709c6
IZUMI1208 <98645501+IZUMI1208@users.noreply.github.com>
2022-01-29 14:44:24 +0300

    Update and rename examples/desktop_authenticator.py to примеры/desktop_authenticator.py

---
+++ примеры/desktop_authenticator.py
@@ -0,0 +1,13 @@
+# Example of generating authenticating codes using guard module
+# To use is you have to obtain 'shared_secret' and 'identity_secret'
+# From your Steamguard file
+
+from steampy.guard import generate_confirmation_key, generate_one_time_code
+shared_secret = 'jSFM+54e0U6wmyDYGTMFQyu4LCs='
+identity_secret = 'LgW1o15aeJmwnRO8C/pwgO75/4Q='
+
+one_time_authentication_code = generate_one_time_code(jSFM+54e0U6wmyDYGTMFQyu4LCs=)
+print(one_time_authentication_code)
+
+confirmation_key = generate_confirmation_key(LgW1o15aeJmwnRO8C/pwgO75/4Q=, 'conf')
+print(confirmation_key)
